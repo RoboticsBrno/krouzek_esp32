70ee4e253be75782ae619d87e948181241a38c98
Vojtěch Boček <vojtech.bocek@avast.com>
2021-03-21 11:54:10 +0100

    Fix 20200305_lorris project

--- 20200305_lorris/src/main.cpp
+++ 20200305_lorris/src/main.cpp
@@ -5,16 +5,20 @@
 #include <mutex>
 #include <math.h>
 
+#include <driver/i2c.h>
+
 #include "packet.h"
 
-enum {
+enum
+{
     CMD_SET_PARAMS = 0,
     CMD_SIN = 1,
     CMD_COS = 2,
     CMD_TAN = 3,
 };
 
-struct data_params {
+struct data_params
+{
     uint32_t tick_rate_ms;
     double step;
 };
@@ -29,31 +33,38 @@ static BluetoothSerial SerialBT;
 static PacketParser gParser;
 
 static void dataThread();
-static void processPacket(const Packet& pkt);
+static void processPacket(const Packet &pkt);
 
 // Vyberte jeden
-static Stream& IO = Serial;
+static Stream &IO = Serial;
 //static Stream& IO = SerialBT;
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     SerialBT.begin("esplorris");
 
     std::thread(dataThread).detach();
 }
 
-void loop() {
-    while(IO.available() > 0) {
-        if(gParser.addByte(IO.read())) {
+void loop()
+{
+    while (IO.available() > 0)
+    {
+        if (gParser.addByte(IO.read()))
+        {
             processPacket(gParser.packet());
         }
     }
     delay(10);
 }
 
-static void processPacket(const Packet& pkt) {
-    switch(pkt.command()) {
-    case CMD_SET_PARAMS: {
+static void processPacket(const Packet &pkt)
+{
+    switch (pkt.command())
+    {
+    case CMD_SET_PARAMS:
+    {
         gParamsMutex.lock();
         gParams.tick_rate_ms = pkt.read<uint32_t>();
         gParams.step = pkt.read<double>();
@@ -63,10 +74,12 @@ static void processPacket(const Packet& pkt) {
     }
 }
 
-static void dataThread() {
+static void dataThread()
+{
     double value = 0;
     Packet pkt;
-    while(true) {
+    while (true)
+    {
         gParamsMutex.lock();
         const data_params params = gParams;
         gParamsMutex.unlock();
